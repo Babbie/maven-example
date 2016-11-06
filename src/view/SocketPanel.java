7c08660b52d304bd3db36a8263d56054bc6da142
Sebastian Wehkamp <sebastianwehkamp@gmail.com>
2016-11-06 13:43:38 +0100

    Kwaliteit toegevoegd

--- src/view/SocketPanel.java
+++ src/view/SocketPanel.java
@@ -3,20 +3,61 @@ package view;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseMotionAdapter;
 
 /**
  * Created by Bab on 26-10-2016.
  */
+
 public class SocketPanel extends JPanel {
+
+    private int squareX = 50;
+    private int squareY = 50;
+    private int squareW = 20;
+    private int squareH = 20;
+
+    public SocketPanel() {
+
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                moveSquare(e.getX(), e.getY());
+            }
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                moveSquare(e.getX(),e.getY());
+            }
+        });
+    }
+
+    private void moveSquare(int x, int y) {
+        int OFFSET = 1;
+        if ((squareX!=x) || (squareY!=y)) {
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+            squareX=x;
+            squareY=y;
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+        }
+    }
 
     @Override
     public void paintComponent(Graphics g) {
         try {
+            super.paintComponent(g);
             BufferedImage beer = ImageIO.read(Thread.currentThread().getContextClassLoader().getResourceAsStream("dikkestinkbeer.jpg"));
             g.drawImage(beer, 0, 0, null);
+            g.setColor(Color.RED);
+            g.fillRect(squareX,squareY,squareW,squareH);
+            g.setColor(Color.BLACK);
+            g.drawRect(squareX,squareY,squareW,squareH);
         } catch (IOException e) {
             e.printStackTrace();
         }
