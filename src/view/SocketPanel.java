9b891e2879edfea0479ba156cd632fe4e5e6b16d
Sebastian Wehkamp <sebastianwehkamp@gmail.com>
2016-11-16 15:25:44 +0100

    bewegende spikkel

--- src/view/SocketPanel.java
+++ src/view/SocketPanel.java
@@ -3,14 +3,17 @@ package view;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseMotionAdapter;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 
 /**
  * Created by Bab on 26-10-2016.
@@ -18,36 +21,39 @@ import java.awt.event.MouseMotionAdapter;
 
 public class SocketPanel extends JPanel {
 
-    private int squareX = 50;
-    private int squareY = 50;
-    private int squareW = 20;
-    private int squareH = 20;
+    private int panelWidth, panelHeight;
+    private int circleX = 0;
+    private int circleY = 0;
+    private int radius = 20;
+    private boolean timerStarted = FALSE;
+    Timer timer;
 
     public SocketPanel() {
-
+        timer = new Timer(100,animator);
         addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
-                moveSquare(e.getX(), e.getY());
-                System.out.print("X: " + e.getX() + "Y: " + e.getY() + "\n");
-            }
-        });
-
-        addMouseMotionListener(new MouseAdapter() {
-            public void mouseDragged(MouseEvent e) {
-                moveSquare(e.getX(),e.getY());
+                if(timerStarted){
+                    timer.stop();
+                    timerStarted=FALSE;
+                } else {
+                    timer.start();
+                    timerStarted=TRUE;
+                }
             }
         });
     }
 
-    private void moveSquare(int x, int y) {
-        int OFFSET = 1;
-        if ((squareX!=x) || (squareY!=y)) {
-            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
-            squareX=x;
-            squareY=y;
-            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+    public void moveCircle(int X, int Y, int speed, String text){
+
+    }
+
+    ActionListener animator = new ActionListener() {
+        public void actionPerformed(ActionEvent evt){
+            repaint(circleX, circleY, radius, radius);
+            circleX+=10;
+            repaint(circleX, circleY, radius, radius);
         }
-    }
+    };
 
     @Override
     public void paintComponent(Graphics g) {
@@ -55,10 +61,14 @@ public class SocketPanel extends JPanel {
             super.paintComponent(g);
             BufferedImage beer = ImageIO.read(Thread.currentThread().getContextClassLoader().getResourceAsStream("dikkestinkbeer.jpg"));
             g.drawImage(beer, 0, 0, null);
+            if (circleX == panelWidth){
+                circleX=0;
+            }
             g.setColor(Color.RED);
-            g.fillRect(squareX,squareY,squareW,squareH);
+            g.fillOval(circleX,circleY,radius,radius);
             g.setColor(Color.BLACK);
-            g.drawRect(squareX,squareY,squareW,squareH);
+            g.drawOval(circleX,circleY,radius,radius);
+            panelWidth = getWidth();
         } catch (IOException e) {
             e.printStackTrace();
         }
