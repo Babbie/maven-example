3e68c1b8f2524dc1474415af6fb90f531b2a3455
Marc Babtist <marcbabtist@gmail.com>
2017-01-05 16:23:22 +0100

    Undo mistake in CircleList

--- src/model/CircleList.java
+++ src/model/CircleList.java
@@ -9,8 +9,14 @@ import java.util.Iterator;
 public class CircleList {
     private static ArrayList<Circle> arrayList;
 
+    private static CircleList circlelist = new CircleList();
+
     private CircleList() {
         arrayList = new ArrayList<>();
+    }
+
+    public static CircleList getInstance(){
+        return circlelist;
     }
 
     public static ArrayList<Circle> getCircleList() {
@@ -19,6 +25,11 @@ public class CircleList {
 
     //Add circle to the list
     public static void addCircle(Circle circle){arrayList.add(circle);}
+
+    //Remove circle from list
+    public static void removeCircle(Circle circle) {
+        arrayList.remove(circle);
+    }
 
     //Loop over the circles and call update on each
     public synchronized static void updateCircles(){
